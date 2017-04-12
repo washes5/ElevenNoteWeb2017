bd61b35daf5efaa1f4b6bc45925567a1ee9db7a5
Angela Washburn <angelawashburn@Angelas-MacBook-Pro-2.local>
2017-04-12 11:53:26 -0400

    Identity Models

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -3,6 +3,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ElevenNote.Data
 {
@@ -29,5 +31,36 @@ namespace ElevenNote.Data
         {
             return new ApplicationDbContext();
         }
-    }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Conventions
+				.Remove<PluralizingTableNameConvention>();
+
+			modelBuilder
+				.Configurations
+				.Add(new IdentityUserLoginConfiguratiion())
+				.Add(new IdentityUserRoleConfiguration());
+		}
+	}
+
+	public class IdentityUserLoginConfiguratiion
+		: EntityTypeConfiguration<IdentityUserLogin>
+	{
+		public IdentityUserLoginConfiguratiion()
+		{
+			HasKey(iul => iul.UserId);
+		}
+	}
+
+	public class IdentityUserRoleConfiguration
+		: EntityTypeConfiguration<IdentityUserRole>
+	{
+		public IdentityUserRoleConfiguration()
+		{
+			HasKey(iur => iur.RoleId);
+		}
+	}
+
 }
